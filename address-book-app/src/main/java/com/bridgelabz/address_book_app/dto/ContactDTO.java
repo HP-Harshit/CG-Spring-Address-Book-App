5cf3c2782b0aad5321340b707c5452cd7e648663
HP-Harshit <harshitpatel274@gmail.com>
2025-02-24 23:38:07 +0530

    [Harshit] feat: Implement UC6 - Use Lombok Library to auto-generate getters and setters for ContactDTO ✨

--- address-book-app/src/main/java/com/bridgelabz/address_book_app/dto/ContactDTO.java
+++ address-book-app/src/main/java/com/bridgelabz/address_book_app/dto/ContactDTO.java
@@ -1,41 +1,11 @@
 package com.bridgelabz.address_book_app.dto;
 
+import lombok.Data;
+
+@Data
 public class ContactDTO {
     private String name;
     private String phoneNumber;
     private String email;
     private String address;
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
